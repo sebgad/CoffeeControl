b10ff2715d06cc427581663e27d1b727a13001a2
hamster-fiedel <hamster-fiedel@gmx.de>
2021-09-10 19:21:29 +0200

    adapted PID lib: fixed controller calculation; other coefficients; added OnOffThresholds

--- PidCtrl.h
+++ PidCtrl.h
@@ -4,8 +4,10 @@
  *  u(t):   manipulated variable (Stellgröße)
  *  e(t):   Deviation between actual value and target value (Regelabweichung)
  *  Kp:     P-Gain (P-Vestärkung)
- *  Tn:     reset time (Nachstellzeit)
- *  Tv:     lead time (Vorhaltzeit)
+ *  Tn:     reset time (Nachstellzeit) Tn = Kp/Ki
+ *  Tv:     lead time (Vorhaltzeit) Tv= Kd/Kp
+ * 
+ * THem constants are stored in the memeber ptrConstants as Kp; 1/Tn; Tv
  */
 
 #ifndef PidCtrl_h
@@ -23,6 +25,8 @@ class PidCtrl
     void changePidCoeffs(float, float, float);
     void changePidCoeffs(const float[][4], size_t);
     void changeTargetValue(float);
+    void setOnOffThresh(float f_tresh_on, float f_tresh_off);
+    void setOnOffThresh(float f_tresh);
     void compute();
     void compute(const float &, float &);
     void addOutputLimits(const float, const float);
@@ -34,9 +38,12 @@ class PidCtrl
     float _fTargetValue;
     float _fLoLim;
     float _fUpLim;
+    float _fTreashOn;
+    float _fTreashOff;
     int _iLinkMode;
     float _fSumIntegrator;
     unsigned long _iLastComputeMillis;
+    float _fLastControlDev;
     size_t _iSizeCoeffTbl;
     void _calcControlEquation();
     void _initCoeffTable(size_t);
