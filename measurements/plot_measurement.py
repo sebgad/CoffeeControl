9d50b67baaa544c6c90401778e08fcdef9025c42
hamster-fiedel <hamster-fiedel@gmx.de>
2022-01-04 12:40:49 +0100

    default python script; added to git ignore

--- measurements/plot_measurement.py
+++ measurements/plot_measurement.py
@@ -8,46 +8,57 @@ Created on Mon Dec 20 19:13:36 2021
 import pandas as pd
 import scipy.signal
 import matplotlib.pyplot as plt
+import os
 import numpy as np
 
-strFname = r"rancilio_silvia_stock_measurement.csv"
 
-# read in csv
-df = pd.read_csv(strFname, skiprows=2, index_col=0)
+lstFname = ["rancilio_silvia_stock_measurement.csv"
+            ]
 
-# apply butter bandpass filter / LPZ filter with no phase shift
+obj_f, ax = plt.subplots(nrows=2, sharex=True)
+ax_pwm = ax[0].twinx()
+f_target = 90.0
+ax[1].axhline(y=0, color='black', linestyle='dashdot')
 
-# filter coefficients
-# N=8: Filter order / amount of coefficients
-# Wn=0.075: Cutoff Frequency as a factor of the Nyquist frequency
-f_b, f_a = scipy.signal.butter(N=10, Wn=0.1)
-# bandpass forward+backward to eleminate phase shift
-df['TempSmoothed'] = scipy.signal.filtfilt(f_b, f_a, df['Temperature'])
+for strFname in lstFname:
+    strFname_wo_ext = os.path.basename(strFname).split(".")[0]
 
-# create plot objects
-obj_f, lst_ax = plt.subplots(nrows=2, sharex=True)
-df.plot(y=['Temperature', 'TempSmoothed'], ax=lst_ax[0])
+    # read in csv
+    df = pd.read_csv(strFname, skiprows=2, index_col=0)
 
-# differential
-df['TempSmoothedDiff^1'] = df['TempSmoothed'].diff(1)
-df['TempSmoothedDiff^2'] = df['TempSmoothed'].diff(2)
+    # apply butter bandpass filter / LPZ filter with no phase shift
 
-df.plot(y=['TempSmoothedDiff^1'], ax=lst_ax[1])
-df.plot(y=['TempSmoothedDiff^2'], ax=lst_ax[1])
+    # filter coefficients
+    # N=8: Filter order / amount of coefficients
+    # Wn=0.075: Cutoff Frequency as a factor of the Nyquist frequency
+    f_b, f_a = scipy.signal.butter(N=10, Wn=0.1)
+    # bandpass forward+backward to eleminate phase shift
+    df['TempSmoothed'] = scipy.signal.filtfilt(f_b, f_a, df['Temperature'])
 
-# calculate regression (1st order) between 120s and 170s
-obj_idx = (df.index>120) & (df.index<170)
+    # create plot objects
+    df.plot(y='Temperature', legend=False, ax=ax[0], label=strFname_wo_ext+"_Temp")
 
-# coefficients of the regression line
-lst_coeffs = np.polyfit(df[obj_idx].index, df[obj_idx]['Temperature'], deg=1)
+    df.plot(y='TargetPWM', ax=ax_pwm, legend=False, label=strFname_wo_ext+"_TargetPWM")
 
-# x points for plot regression line
-xp = np.arange(0, 275, 5)
-lst_ax[0].plot(xp, np.polyval(lst_coeffs, xp))
-# df[idx].plot(x='Time', y='Temperature', ax=ax)
+    df['TemperatureDeviation'] = f_target - df['TempSmoothed']
+    df.plot(y='TemperatureDeviation', label=strFname_wo_ext+"_TempDiffTgt", ax=ax[1], legend=False)
 
-lst_ax[0].grid(True)
-lst_ax[1].grid(True)
-lst_ax[0].set_yticks(range(0,120,10))
-lst_ax[0].set_xlim(0, 400)
-lst_ax[0].set_ylabel('Temperature')
+ax[0].grid(True)
+ax[0].set_ylim(0, 130)
+ax_pwm.set_ylim(0, 260)
+
+ax[0].set_yticks(np.arange(0,135,5))
+ax_pwm.set_yticks(np.arange(0,265,10))
+ax[0].set_ylabel('Temperature')
+ax_pwm.set_ylabel('TargetPWM')
+
+ax[1].set_ylim(-10, 10)
+ax[1].grid(True)
+ax[1].set_yticks(range(-10,11))
+ax[1].set_ylabel("Temperature Deviation to Target")
+
+
+obj_f.legend(loc='lower right', ncol=4)
+plt.tight_layout()
+obj_f.subplots_adjust(hspace=0.05)
+
